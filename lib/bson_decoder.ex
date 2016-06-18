f59687d7c4c9db01dd881f88f34b5ef0d65937af
nnydjesus <nnydjesus@gmail.com>
2016-06-17 23:33:06 -0300

    fix

--- lib/bson_decoder.ex
+++ lib/bson_decoder.ex
@@ -51,7 +51,7 @@ defmodule Bson.Decoder do
 
   ```
   """
-  def elist_to_hashdict(elist), do: elist |> Enum.reduce %HashDict{}, fn({k, v}, h) -> HashDict.put(h, k, v) end
+  def elist_to_hashdict(elist), do: elist |> Enum.reduce(%HashDict{}, fn({k, v}, h) -> HashDict.put(h, k, v) end)
 
   @doc """
   Transform an elist to a Keyword
@@ -62,7 +62,7 @@ defmodule Bson.Decoder do
 
   ```
   """
-  def elist_to_keyword(elist), do: elist |> Enum.map fn({k, v}) -> {String.to_atom(k), v} end
+  def elist_to_keyword(elist), do: elist |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
 
   @doc """
   Identity function
